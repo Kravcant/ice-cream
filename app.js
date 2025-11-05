403a862819021ae8145693d8c2ba470468e927d5
anthonykravchishin <tonykravc53@gmail.com>
2025-11-04 19:00:20 -0800

    Testing to see if this works

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const PORT = 3001;
 
 app.set('view engine', 'ejs');
 
-// app.use(express.urlencoded({ extended: true }));
-
 const orders = [];
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    // res.sendFile(`${import.meta.dirname}/views/home.html`);
     res.render('home');
 });
 
