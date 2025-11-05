645a947d001355851679376e9dc1802b668af018
anthonykravchishin <tonykravc53@gmail.com>
2025-11-04 20:28:08 -0800

    Fixing error

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = 3001;
 
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const orders = [];
