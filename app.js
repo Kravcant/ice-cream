fa61118027b66a504f88962eb7511395ac2be790
anthonykravchishin <tonykravc53@gmail.com>
2025-11-04 19:39:14 -0800

    Fixing error

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3001;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 console.log('CWD:', process.cwd());
 console.log('View engine:', app.get('view engine'));
